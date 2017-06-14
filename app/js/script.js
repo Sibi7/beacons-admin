d065499d618295da31643d5f5ed15a76997ae4a7
Vladimir__Lipodat <sibi_7@mail.ru>
2017-06-14 20:49:07 +0300

    responsive

--- app/js/script.js
+++ app/js/script.js
@@ -1,42 +1,34 @@
 $(document).ready(function () {
 
-    $(document).on('click','.sidebar__head__hamburger', function () {
+    $(document).on('click', '.sidebar__head__hamburger', function () {
         $('.sidebar').toggleClass('actives');
 
     });
-
-    // $(".sidebar__head__hamburger").click(function () {
-    //     $('.sidebar').toggleClass("actives");
-    // });
-
 
     $(".sidebar__head__hamburger").click(function () {
         $('.content').toggleClass("active-content");
     });
 
 
-    $( ".content__header__search a" ).click(function() {
-        $( ".content__header__search input" ).toggle( "slow", function() {
+    $(".content__header__search a").click(function () {
+        $(".content__header__search input").toggle("slow", function () {
             // Animation complete.
         });
     });
-
-    console.log($(window).width());
-
-    changeSidebar();
 
     $(window).resize(function () {
         changeSidebar();
     });
 
     function changeSidebar() {
-        if ($(window).width() >= 750){
+        if ($(window).width() >= 990) {
             $('.sidebar').removeClass("actives");
         }
-        else{
+        else {
             $('.sidebar').addClass("actives");
         }
     }
 
+    changeSidebar();
 });
 
